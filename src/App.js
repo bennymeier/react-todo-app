6ce1dd70ef85fcc2f8739db30c7537fc6546f2fc
Benny Meier <benjamin.meier@unitedplanet.com>
2019-06-04 09:59:23 +0200

    Fix some bugs, like 'key is undefined', create some methods for a better scheme, add the method to delete completed todos

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ class App extends React.Component {
     const todos = [];
     Object.keys(localStorage).forEach((key) => todos.push({ key, ...JSON.parse(localStorage.getItem(key)) }));
     this.state = { todos: [...todos] };
-    console.log(this.state);
+    this.todoField = React.createRef();
 
   }
   render() {
     return (
       <div className="container">
         <h1>Add todos <span role="img" aria-label="Document">📝</span></h1>
-        <input className="todo-input" onKeyDown={this.checkKey.bind(this)} placeholder="Your todo..." type="text" id="add" />
+        <input className="todo-input" ref={this.todoField} onKeyDown={this.checkKey.bind(this)} placeholder="Your todo..." type="text" id="add" />
         <button className="add-btn" onClick={this.add.bind(this)}> <span role="img" aria-label="Handshake">👋🏻</span></button>
         <h2>Open Todos</h2>
         <ul>
@@ -35,7 +35,7 @@ class App extends React.Component {
     );
   }
   componentDidMount() {
-    this.todoField = document.getElementById("add");
+    this.todoField.current.focus();
   }
   checkKey(e) {
     if (e.key === "Enter" || e.keyCode === 13) {
@@ -43,44 +43,63 @@ class App extends React.Component {
     }
   }
   async add() {
-    const todo = { text: this.todoField.value, datetime: new Date().toLocaleString(), isDone: false };
-    const key = this.uniqueId();
-    const todoWithKey = { ...todo, key }
-    this.setState({ todos: [...this.state.todos, todoWithKey] }, async () => await this.addToLocalStorage(key, todo));
-    this.todoField.value = "";
+    return new Promise((resolve) => {
+      const todo = { text: this.todoField.current.value, datetime: new Date().toLocaleString(), isDone: false };
+      const key = this.uniqueId();
+      const todoWithKey = { ...todo, key }
+      this.setState({ todos: [...this.state.todos, todoWithKey] }, async () => await this.addToLocalStorage(key, todo));
+      this.clearInput();
+      resolve();
+    });
   }
   async done(key) {
-    // const key = e.target.getAttribute("data-key");
-    let todos = [...this.state.todos];
-    for (const todo of todos) {
-      if (todo.key === key) {
-        todo.isDone = true;
-        await this.updateLocalStorage(todo.key, todo);
-        console.log("1: ", this.state);
-        this.setState((prev) => ({ todos: [...prev.todos] }));
-        console.log("2: ", this.state);
+    return new Promise(async (resolve, reject) => {
+      if (!key) reject(`key is ${key}`);
+      let todos = [...this.state.todos];
+      for (const todo of todos) {
+        if (todo.key === key) {
+          todo.isDone = true;
+          await this.updateLocalStorage(todo.key, todo);
+          this.setState((prev) => ({ todos: [...prev.todos] }));
+          resolve();
+        }
       }
-    }
+    });
+  }
+  delete(key) {
+    return new Promise((resolve, reject) => {
+      if (!key) reject(`key is ${key}`);
+      let todos = [...this.state.todos];
+      const newTodos = todos.filter((todo) => todo.key !== key);
+      this.setState(({ todos: [...newTodos] }));
+      resolve();
+    });
   }
   addToLocalStorage(key, todo) {
     return new Promise((resolve) => {
+      if (!key) throw new Error(`key is ${key}`);
       localStorage.setItem(key, JSON.stringify(todo));
       resolve();
     });
   }
   updateLocalStorage(key, todo) {
-    console.log(todo);
     return new Promise((resolve) => {
-      if (todo.key) delete todo.key;
+      if (!key) throw new Error(`key is ${key}`);
+      // if (todo.key) delete todo.key;
       localStorage.setItem(key, JSON.stringify(todo));
       resolve();
     });
   }
-  deleteFromLocalStorage(e) {
-    return new Promise((resolve) => {
-      const key = e.target.getAttribute("data-key");
-      if (key) localStorage.removeItem(key);
+  deleteFromLocalStorage(key) {
+    return new Promise(async (resolve) => {
+      if (!key) throw new Error(`key is ${key}`);
+      await this.delete(key);
+      localStorage.removeItem(key);
+      resolve();
     });
+  }
+  clearInput() {
+    this.todoField.current.value = "";
   }
   uniqueId() {
     const ID_LENGTH = 36;
